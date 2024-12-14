6a6c37f797522197a31e29226b3be3384be5dff3
kazou92 <99810202+kazou92@users.noreply.github.com>
2024-12-14 04:45:43 +0000

    arrleglando el projecto borrando el index y hacer todos en app.js y renderizando el resultado en la pagina como  usted me lo habia indicado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  const combinateDomaine = (pron, adj, noun) => {
+    let domaines = [];
+    for (let i = 0; i < pron.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          domaines.push(`${pron[i]}${adj[j]}${noun[k]}.com`);
+        }
+      }
+    }
+    return domaines;
+  };
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let result = combinateDomaine(pronoun, adj, noun);
+
+  let domaine = document.querySelector(".domaine");
+
+  let ol = document.createElement("ol");
+
+  for (let r of result) {
+    let li = document.createElement("li");
+
+    li.innerHTML = r;
+    ol.appendChild(li);
+    domaine.appendChild(ol);
+  }
+  //console.log(result.join("\n"));
 };
